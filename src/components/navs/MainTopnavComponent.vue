6aad1711beecca604b181721f79dc46c7f8ad0cc
devUgur <ugur44ist@gmail.com>
2024-05-10 17:38:34 +0200

    vv

--- src/components/navs/MainTopnavComponent.vue
+++ src/components/navs/MainTopnavComponent.vue
@@ -17,7 +17,7 @@ const handleDropdown = function(dropdown: any){
 
 <template>
   <div class="fixed top-0  w-full bg-opacity-90" :class="{'full-open':dropdownIsOpen}" @mouseleave="dropdownIsOpen = false">
-    <div class="flex h-24 place-items-center items-center justify-between lg:justify-center">
+    <div class="flex bg-white h-24 place-items-center items-center justify-between lg:justify-center">
       <div class="mr-6 text-xl"> Logo </div>
          <div class="hidden lg:flex space-x-10 text-xs text-black">
            <div v-for="link in MainLinks" :key="link.name" @mouseenter="handleDropdown(link)" class="">{{ link.name }}</div>
@@ -29,7 +29,7 @@ const handleDropdown = function(dropdown: any){
          </div>
     </div>
     <div class="" :class="{'full-open': dropdownIsOpen}">
-      <div id="dropdown" :class="{'open': dropdownIsOpen}" class="bg-red-200">
+      <div id="dropdown" :class="{'open': dropdownIsOpen}" class="bg-white">
       {{ dropdownContent }}
     </div>
     <div v-if="dropdownIsOpen" class="h-full backdrop-blur-sm"  @mouseenter="dropdownIsOpen = false"></div>
@@ -41,7 +41,7 @@ const handleDropdown = function(dropdown: any){
 <style scoped>
 #dropdown{
   height: 0vh;
-  transition: height 0.5s;
+  transition: height 0.3s;
   overflow: hidden
 }
 .full-open{
